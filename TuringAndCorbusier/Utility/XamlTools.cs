d471f31d1c86c41cd3cb0f0473dfdce07d01b922
minho <prefkha@gmail.com>
2017-06-01 03:00:24 +0900

    add files

---
+++ TuringAndCorbusier/Utility/XamlTools.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TuringAndCorbusier.Utility
+{
+    class XamlTools
+    {
+        public static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+
+        public static class ResourceHelper
+        {
+            static public string FindNameFromResource(ResourceDictionary dictionary, object resourceItem)
+            {
+                foreach (object key in dictionary.Keys)
+                {
+                    if (dictionary[key] == resourceItem)
+                    {
+                        return key.ToString();
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
