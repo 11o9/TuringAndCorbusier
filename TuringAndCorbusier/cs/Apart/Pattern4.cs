a91cd2c5861a6f6423b0e1a4ae5c6fafc17087f5
jhlee <jhlee@spacewalk.tech>
2017-08-23 16:59:12 +0900

    SH20170823B

--- TuringAndCorbusier/cs/Apart/Pattern4.cs
+++ TuringAndCorbusier/cs/Apart/Pattern4.cs
@@ -231,8 +231,8 @@ namespace TuringAndCorbusier
         private double[] maxInput = { TuringAndCorbusierPlugIn.InstanceClass.page1Settings.MaxFloors - 1, TuringAndCorbusierPlugIn.InstanceClass.page1Settings.MaxFloors - 1, CoreType.Vertical.GetDepth() + 2000, 2 * Math.PI, 1 };
 
         //Parameter GA최적화 {mutation probability, elite percentage, initial boost, population, generation, fitness value, mutation factor(0에 가까울수록 변동 범위가 넓어짐)
-        //private double[] GAparameterset = { 0.8, 0.05, 1, 20, 4, 10, 1 }; //원본
-        private double[] GAparameterset = { 0.2, 0.03, 1, 5, 1, 3, 1 };
+        private double[] GAparameterset = { 0.8, 0.05, 1, 20, 4, 10, 1 }; //원본
+        //private double[] GAparameterset = { 0.2, 0.03, 1, 5, 1, 3, 1 };
         //private double[] GAparameterset = { 0.2, 0.03, 1, 100, 5, 3, 1 };
 
         public override string GetAGType
